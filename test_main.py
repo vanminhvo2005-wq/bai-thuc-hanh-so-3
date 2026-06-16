8e832bb19bf9abe113292f3bd2abf77221afba2d
Ngoc Tram Nguyen <ngoctram21276667acb@gmail.com>
2026-06-16 17:10:39 +0700

    chore: cấu hình hệ thống tự động kiểm thử GitHub Actions

--- test_main.py
+++ test_main.py
@@ -1,10 +1,13 @@
 import unittest
 from main import tinh_diem_gpa
-class TestTinhDiemGPA(unittest.TestCase):
-    def test_tinh_diem_gpa(self):
+
+class TestGpaFunctions(unittest.TestCase):
+
+    def test_gpa_he_muoi(self):
         self.assertEqual(tinh_diem_gpa(8.5), 4.0)
+
+    def test_gpa_tuyen_tinh(self):
         self.assertEqual(tinh_diem_gpa(7.0), 2.8)
-        self.assertEqual(tinh_diem_gpa(9.0), 4.0)
-        self.assertEqual(tinh_diem_gpa(6.5), 2.6)   
+
 if __name__ == '__main__':
     unittest.main()
